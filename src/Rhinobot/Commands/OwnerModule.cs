5e91e37a63e1893cb5dfd638484e1b328e53fecc
IsaTippens <isatippens2@gmail.com>
2021-01-16 23:42:45 +0200

    Update command hopefully fires sh script

--- src/Rhinobot/Commands/OwnerModule.cs
+++ src/Rhinobot/Commands/OwnerModule.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using System.Diagnostics.Process;
 
 using Discord;
 using Discord.Commands;
@@ -9,6 +10,7 @@ public class OwnerModule : ModuleBase<SocketCommandContext>
     [Command("Update")]
     public async Task UpdateAsync([Remainder] string message = null)
     {
-        await ReplyAsync("UwU");
+        await ReplyAsync("Updating...");
+        var p = Process.Start("/home/isatippens2/rhinohome/RhinoBot/update");
     }
 }
